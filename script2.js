e67fcd62774e578273be162cf471f0eb2caa4eb8
shreetoma <shreetoma14@gmail.com>
2018-01-01 23:36:39 +0530

    by shree

--- script2.js
+++ script2.js
@@ -96,8 +96,79 @@ function completedTask(){
     completed=(document.getElementById("taskscompleted").getElementsByTagName("li").length);
     document.getElementById("completedtask").innerHTML=completed;
 }
-function taskDefault(){
+/*function taskDefault(){
     alert("Hello Shreetoma!!.. Start ur todo..");
  document.getElementById("pendingtask").innerHTML=pending;
 document.getElementById("completedtask").innerHTML=completed;
+}*/
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    console.log(today);
+    h=checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("clock").innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 3600000);
+    storage();
+   
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i}; 
+    return i;
+}
+function logIn(){
+    location.href="/index17.html";
+    console.log("Hello");
+   // localStorage.name="shree";
+    if(document.getElementById("userName").value=="Shree" && document.getElementById("pwd").value=="1234"){
+localStorage.name=document.getElementById("userName").value;
+console.log("Welcome back" + localStorage.name);
+    //document.getElementById("store").innerHTML= "Welcome back" + localStorage.name;
+}
+    else{
+        alert("Wrong user ID");
+    }
+}
+function storage(){
+     document.getElementById("store").innerHTML= "Welcome back   " + localStorage.name;
+}
+function weather (){
+    var url = 'http://api.apixu.com/v1/current.json?key=23319fb777b04d468cc173118173112&q=Pune'
+    fetch(url)
+  .then((resp) => resp.json())
+  .then(function(data) {
+        
+        console.log(data.current);
+       
+        //console.log(document.getElementById("weatherUl").hasChildNodes);
+        
+        var u=document.createElement("li");
+        var location= document.createElement("span");
+        var condition= document.createElement("span");
+        var condition1= document.createElement("span");
+        var temp= document.createElement("span");
+        var humidity= document.createElement("span");
+        var label2=document.createElement("label");
+        location.innerText=data.current;
+        condition.innerText=data.current.condition.text;
+        condition1.innerText=data.current.condition.icon;
+        temp.innerText=data.current.temp_c;
+        humidity.innerText=data.current.humidity;
+        u.appendChild(condition);
+         u.appendChild(condition1);
+         u.appendChild(temp);
+         u.appendChild(humidity);
+        document.getElementById("weather").appendChild(u);
+        
+    
+    })
+  
+  .catch(function(error) {
+        
+    console.log(error);
+  });  
 }
